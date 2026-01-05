322646d6c97991d7c47c65ce6c623aa7f5956db0
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2026-01-05 14:02:01 +0100

    registration and login process done

--- task-fe/src/app/components/nav/nav.ts
+++ task-fe/src/app/components/nav/nav.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterLinkActive, RouterLink } from '@angular/router';
+import { Company, User } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-nav',
@@ -14,5 +15,27 @@ export class NavComponent {
 
   goToRoute(route: string) {
     this.router.navigate([`${route}`]);
+  }
+  logout() {
+    localStorage.clear();
+    this.authService.setIsLoggedIn(false);
+    this.authService.setCompany(null);
+    this.authService.setUser(null);
+    this.authService.setAccessToken(null);
+    this.authService.setRefreshToken(null);
+    this.router.navigate(['home/login']);
+  }
+  getImage(): string {
+    var image: string = '';
+    if (this.authService.getUser()) {
+      image = (this.authService.getUser() as User)?.immagine?.filter((i) => i.isCurrent)[0]?.image;
+    } else if (this.authService.getCompany()) {
+      image = (this.authService.getCompany() as Company)?.immagine?.filter((i) => i.isCurrent)[0]
+        ?.image;
+    }
+    if (!image || (image && image.length == 0)) {
+      image = '/assets/logo/logo.png';
+    }
+    return image;
   }
 }
