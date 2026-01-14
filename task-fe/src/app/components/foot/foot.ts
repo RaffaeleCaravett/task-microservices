492edb81b91031483e2f3cf536c6ec6901e3d3bf
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2026-01-14 22:35:35 +0100

    restyling everything

--- task-fe/src/app/components/foot/foot.ts
+++ task-fe/src/app/components/foot/foot.ts
@@ -1,6 +1,7 @@
 import { Component, effect, inject } from '@angular/core';
 import { ModeService } from '../../services/mode.service';
 import { NgClass } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-foot',
@@ -11,7 +12,7 @@ import { NgClass } from '@angular/common';
 export class FootComponent {
   isDark: boolean = false;
   protected modeService: ModeService = inject(ModeService);
-
+  protected authService: AuthService = inject(AuthService);
   constructor() {
     effect(() => {
       this.isDark = this.modeService.isDark();
