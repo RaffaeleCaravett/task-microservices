364d5df0524441bc9f8bd6d76967fa02ccd83afd
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2026-01-31 17:58:06 +0100

    done some other things

--- task-fe/src/app/components/home/home.ts
+++ task-fe/src/app/components/home/home.ts
@@ -1,35 +1,50 @@
 import { NgClass } from '@angular/common';
 import { ChangeDetectorRef, Component, effect, HostListener, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { ModeService } from '../../services/mode.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [ NgClass],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class HomeComponent implements OnInit {
   isDark: boolean = false;
   protected modeService: ModeService = inject(ModeService);
   protected cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+  protected router: Router = inject(Router);
+  homeForm: FormGroup = new FormGroup({});
   ngOnInit(): void {
     this.onResize(null);
+    this.initForm();
   }
 
+  initForm() {
+    this.homeForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/),
+      ]),
+    });
+  }
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
-    console.log(window.innerHeight);
-    let home = document.getElementsByClassName('home')[0] as HTMLDivElement;
-    if (home != null) {
-      home.style.minHeight = window.innerHeight - (window.innerHeight * 25) / 100 + 'px';
+    // let home = document.getElementsByClassName('home')[0] as HTMLDivElement;
+    // if (home != null) {
+    //   home.style.minHeight = window.innerHeight - (window.innerHeight * 25) / 100 + 'px';
+    // }
+  }
+  checkPermission() {
+    if (this.homeForm.valid) {
+      this.router.navigate(['signup', this.homeForm.controls['email'].value]);
     }
   }
-
-  constructor(){
-    effect(()=>{
+  constructor() {
+    effect(() => {
       this.isDark = this.modeService.isDark();
       this.cdr.markForCheck();
-    })
+    });
   }
 }
