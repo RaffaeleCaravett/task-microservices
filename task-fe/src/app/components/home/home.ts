a3e65687d4b26792210c8445abdb576eec4617cf
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2026-02-02 20:55:40 +0100

    building up signup page

--- task-fe/src/app/components/home/home.ts
+++ task-fe/src/app/components/home/home.ts
@@ -13,6 +13,8 @@ import {
 import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { ModeService } from '../../services/mode.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 @Component({
   selector: 'app-home',
@@ -27,7 +29,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
   protected router: Router = inject(Router);
   homeForm: FormGroup = new FormGroup({});
   protected homeCards: any[] = [];
-  canvas = viewChild<HTMLCanvasElement>('canvas');
+  canvas = viewChild<ElementRef<HTMLCanvasElement>>('pencil');
+  model: any;
+  private resizeObserver!: ResizeObserver;
+  private pendingResize = false;
+  private lastWidth = 0;
+  private lastHeight = 0;
   ngOnInit(): void {
     this.onResize(null);
     this.initForm();
@@ -37,9 +44,85 @@ export class HomeComponent implements OnInit, AfterViewInit {
     const canvasEl = this.canvas();
     if (!canvasEl) return;
 
-    console.log(canvasEl); // HTMLCanv
-  }
+    // Scene
+    const scene = new THREE.Scene();
 
+    // Camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      canvasEl.nativeElement.clientWidth / canvasEl.nativeElement.clientHeight,
+      1,
+      10,
+    );
+    camera.position.set(0, 1, 3);
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasEl.nativeElement,
+      antialias: true,
+      alpha: true,
+    });
+
+    // Lights
+    scene.add(new THREE.AmbientLight(0xffffff, 0.8));
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(5, 10, 7);
+    scene.add(dirLight);
+
+    // GLB loader
+    const loader = new GLTFLoader();
+    loader.load(
+      'assets/models/desk.glb',
+      (gltf: any) => {
+        this.model = gltf.scene;
+
+        this.model.scale.set(13, 13, 13);
+        this.model.rotateY(-7);
+        console.log(this.model);
+        scene.add(this.model);
+      },
+      undefined,
+      (error: any) => {
+        console.error('GLB load error', error);
+      },
+    );
+    const container = canvasEl.nativeElement.parentElement!;
+
+    this.resizeObserver = new ResizeObserver((entries) => {
+      const { width, height } = entries[0].contentRect;
+
+      // evita resize inutili
+      if (width === this.lastWidth && height === this.lastHeight) return;
+
+      this.lastWidth = width;
+      this.lastHeight = height;
+      this.pendingResize = true;
+    });
+
+    this.resizeObserver.observe(container);
+    const animate = () => {
+      requestAnimationFrame(animate);
+      this.angle += this.speed;
+      if (this.model) {
+        this.model.position.x = Math.cos(this.angle) * -this.radius;
+        this.model.position.z = Math.sin(this.angle) * (this.radius - 0.3);
+        this.model.position.y = Math.sin(this.angle) * (this.radius - 0.2);
+      }
+      if (this.pendingResize) {
+        this.pendingResize = false;
+
+        camera.aspect = this.lastWidth / this.lastHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(this.lastWidth, this.lastHeight, false);
+      }
+      renderer.render(scene, camera);
+    };
+    animate();
+  }
+  private angle = 45;
+  private radius = 0.5;
+  private speed = 0.01;
   initForm() {
     this.homeForm = new FormGroup({
       email: new FormControl('', [
