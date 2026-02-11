5b025a420864d9df349863645b7c5df6c81e72f6
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2026-02-11 09:00:20 +0100

    changed style

--- task-fe/src/app/app.config.ts
+++ task-fe/src/app/app.config.ts
@@ -10,6 +10,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { AuthInterceptor, DEFAULT_TIMEOUT } from './core/auth.interceptor';
 import { ErrorInterceptor } from './core/error.interceptor';
+import { MessageService } from 'primeng/api';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
@@ -28,5 +29,6 @@ export const appConfig: ApplicationConfig = {
     }),
     provideAnimations(),
     provideToastr(),
+    MessageService,
   ],
 };
--- task-fe/src/app/core/error.interceptor.ts
+++ task-fe/src/app/core/error.interceptor.ts
@@ -10,20 +10,27 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, catchError, throwError } from 'rxjs';
 import { AuthService, SKIP_AUTH_ERROR } from '../services/auth.service';
+import { MessageService } from 'primeng/api';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(
     private toastr: ToastrService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private messageService:MessageService
   ) {}
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((err: any) => {
         if (err instanceof HttpErrorResponse) {
           if (!request.context.get(SKIP_AUTH_ERROR)) {
-            this.toastr.error(err?.error?.message || err?.error?.messages[0]);
+            this.messageService.add({
+            severity: 'error',
+            summary: 'Attention',
+            detail: err?.error?.message || err?.error?.messages[0],
+            life: 3000,
+          });
           }
         }
         return throwError(() => err);
