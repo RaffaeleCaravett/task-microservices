a69f2ca0d9c75dc2b5040e8751a8d2f4d37e4c10
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2026-03-12 11:17:09 +0100

    doing scheduler

---
+++ task-scheduler/src/main/java/com/example/demo/scheduler/SchedulerService.java
@@ -0,0 +1,9 @@
+package com.example.demo.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SchedulerService {
+}
